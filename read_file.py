9c5212c2c69eb2a2050371de65bf5396f480fcda
Daniel Nakolah <oladidi.daniel@gmail.com>
2020-06-04 10:59:12 +0300

    Using with as context manager

--- read_file.py
+++ read_file.py
@@ -1,12 +1,5 @@
 file_location = '/home/isnakolah/Desktop/PythonFundamentals/OOP/File-Handling/text.txt'
 
-handle = open(file_location, 'r')
-
-data = handle.read()
-counter = 0
-
-for word in data.split():
-    if word == 'Python':
-        counter += 1
-
-print(counter)
+with open('file_location', 'r') as handle:
+    data = handle.read()
+    print(data)
